7078ed7881a3a10624f35aa11a946a2f9d9eaf49
ltg <111@222.com>
2020-11-19 08:50:59 +0800

    修改

--- Test_Mall.py
+++ Test_Mall.py
@@ -1,8 +1,8 @@
 import requests
 
 url = "https://appdev.xpandago.com/app/member/login/register/login"
-payload = {'mobile':'18539576908',
-           'password':'123456',
+payload = {'mobile':'185',
+           'password':'****',
            'areaCode':'+86'}
 headers = {'user-agent': 'my-app/0.0.1'}
 r = requests.get(url,params=payload,headers = headers)
