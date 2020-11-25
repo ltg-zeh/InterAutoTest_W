a168bb23177d3db498c91845f903eb56d465909d
ltg <111@222.com>
2020-11-25 14:31:46 +0800

    添加

--- Test_Mall.py
+++ Test_Mall.py
@@ -1,11 +1,38 @@
 import requests
+# 登录
+def login():
+    url = "https://appdev.xpandago.com/app/member/login/register/login"
+    data = {"mobile":"18539576908","password":"123456","areaCode":"+86"}
+    r = requests.get(url,params=data)
+    print(r.text)
+def info():
+    url = "https://appdev.xpandago.com/app/member/user/user/list"
+    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA2MjgyMjEyMjQ3LCJleHAiOjE2MDY4ODcwMTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.6iCaPo7vqVSXUIPM0ciyYpvh25TRZSoqb0YcBtTCob3NzxZ1WuW6sN87NsV7UxrhWHVpEoJfVWb5vi7Cu1nhAA"}
+    r = requests.get(url,headers=headers)
+    print(r.text)
 
-url = "https://appdev.xpandago.com/app/member/login/register/login"
-payload = {'mobile':'185',
-           'password':'****',
-           'areaCode':'+86'}
-headers = {'user-agent': 'my-app/0.0.1'}
-r = requests.get(url,params=payload,headers = headers)
-print(r.url)
-print(r.text)
-# print(r.content)
+def goodlist():
+    url = "https://appdev.xpandago.com/app/goods/brand/brand/goodlist"
+    data = {"brandId":"286","sidx":"person","order":"desc","page":"1"}
+    r = requests.get(url,params=data)
+    print(r.text)
+
+def cart():
+    url = "https://appdev.xpandago.com/app/shop/cart/buycar/saveNew"
+    data = {"goodsId":"3107","num":"1","productId":"2843","goodType":"normal"}
+    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA2MjgyMjEyMjQ3LCJleHAiOjE2MDY4ODcwMTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.6iCaPo7vqVSXUIPM0ciyYpvh25TRZSoqb0YcBtTCob3NzxZ1WuW6sN87NsV7UxrhWHVpEoJfVWb5vi7Cu1nhAA"}
+    r = requests.post(url,data=data,headers=headers)
+    print(r.text)
+
+def orderNo():
+    url = "https://appdev.xpandago.com/app/order/order/order/addOrderToRedisNew"
+    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA2MjgyMjEyMjQ3LCJleHAiOjE2MDY4ODcwMTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.6iCaPo7vqVSXUIPM0ciyYpvh25TRZSoqb0YcBtTCob3NzxZ1WuW6sN87NsV7UxrhWHVpEoJfVWb5vi7Cu1nhAA"}
+    r = requests.get(url,headers=headers)
+    print(r.text)
+
+if __name__ == '__main__':
+    # login()
+    # info()
+    # goodlist()
+    # cart()
+    orderNo()
