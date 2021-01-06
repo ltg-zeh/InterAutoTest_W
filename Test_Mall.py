3894e43ba2783f5e85f6cd7b2982ee557a08ca3c
ltg <111@222.com>
2021-01-06 18:02:51 +0800

    重构

--- Test_Mall.py
+++ Test_Mall.py
@@ -1,4 +1,6 @@
 import requests
+from utils.RequestsUtils import requests_get
+from utils.RequestsUtils import requests_post
 # 登录
 def login():
     url = "https://appdev.xpandago.com/app/member/login/register/login"
@@ -7,9 +9,11 @@ def login():
     print(r.text)
 def info():
     url = "https://appdev.xpandago.com/app/member/user/user/list"
-    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA2MjgyMjEyMjQ3LCJleHAiOjE2MDY4ODcwMTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.6iCaPo7vqVSXUIPM0ciyYpvh25TRZSoqb0YcBtTCob3NzxZ1WuW6sN87NsV7UxrhWHVpEoJfVWb5vi7Cu1nhAA"}
-    r = requests.get(url,headers=headers)
-    print(r.text)
+    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA5OTE2ODkyMzk1LCJleHAiOjE2MTA1MjE2OTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.aE9EVt619DQO-Z8PufAEHuuZRmUzHBKFfaTWWoREj_CdHY1TJQmjROvxKZNJMBULQ9d-ZhslG3L8qqtvyjmu8Q"}
+    # r = requests.get(url,headers=headers)
+    # print(r.text)
+    r = requests_get(url,headers=headers)
+    print(r)
 
 def goodlist():
     url = "https://appdev.xpandago.com/app/goods/brand/brand/goodlist"
@@ -19,10 +23,13 @@ def goodlist():
 
 def cart():
     url = "https://appdev.xpandago.com/app/shop/cart/buycar/saveNew"
-    data = {"goodsId":"3107","num":"1","productId":"2843","goodType":"normal"}
-    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA2MjgyMjEyMjQ3LCJleHAiOjE2MDY4ODcwMTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.6iCaPo7vqVSXUIPM0ciyYpvh25TRZSoqb0YcBtTCob3NzxZ1WuW6sN87NsV7UxrhWHVpEoJfVWb5vi7Cu1nhAA"}
-    r = requests.post(url,data=data,headers=headers)
-    print(r.text)
+    data = {"goodsId":"7238","num":"1","productId":"4697","goodType":"normal"}
+    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA5OTIzNDA5MDk2LCJleHAiOjE2MTA1MjgyMDksInVzZXJfZ3JvdXBfaWQiOm51bGx9.wQCp1A6j235VWE7tyBiZfPvCGVp_1bs8uK5Ei6OQHLRSCgtOwJcOifgY-cRTxGirXyVqKZuWPjMZVm071H7fDg"}
+    # r = requests.post(url,data=data,headers=headers)
+    # print(r.text)
+    # print(r.json())
+    r = requests_post(url,data=data,headers=headers)
+    print(r)
 
 def orderNo():
     url = "https://appdev.xpandago.com/app/order/order/order/addOrderToRedisNew"
@@ -34,5 +41,5 @@ if __name__ == '__main__':
     # login()
     # info()
     # goodlist()
-    # cart()
-    orderNo()
+    cart()
+    # orderNo()
--- utils/RequestsUtils.py
+++ utils/RequestsUtils.py
@@ -1,1 +1,33 @@
+
+import requests
 # 创建封装方法
+def requests_get(url, params=None, **kwargs):
+# 发送requests get请求
+    r = requests.get(url,params=params,**kwargs)
+# 获取相应结果
+    code = r.status_code
+    try:
+        body = r.json()
+    except Exception as e:
+        body = r.text
+# 保存到字典中
+    res = dict()
+    res["code"] = code
+    res["body"] = body
+# 字典返回
+    return res
+
+# post方法封装
+# 创建post方法**kwargs
+def requests_post(url, data=None, json=None, **kwargs):
+    r = requests.post(url, data=data, json=json, **kwargs)
+    code = r.status_code
+    try:
+        body = r.json()
+    except Exception as e:
+        body = r.text
+    # body = r.text
+    res = dict()
+    res["code"] = code
+    res["body"] = body
+    return res
