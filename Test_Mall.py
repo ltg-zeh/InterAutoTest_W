df02955236152a0d2b5b154d7fd65142eaf4c75d
ltg <111@222.com>
2021-01-08 09:49:00 +0800

    重构

--- Test_Mall.py
+++ Test_Mall.py
@@ -1,25 +1,37 @@
 import requests
 from utils.RequestsUtils import requests_get
 from utils.RequestsUtils import requests_post
+from utils.RequestsUtils import Request
 # 登录
 def login():
     url = "https://appdev.xpandago.com/app/member/login/register/login"
     data = {"mobile":"18539576908","password":"123456","areaCode":"+86"}
-    r = requests.get(url,params=data)
-    print(r.text)
+    # r = requests.get(url,params=data)
+    # print(r.text)
+    # r = requests_get(url,params=data)
+    request = Request()
+    r = request.get(url,params=data)
+    print(r)
+
 def info():
     url = "https://appdev.xpandago.com/app/member/user/user/list"
     headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA5OTE2ODkyMzk1LCJleHAiOjE2MTA1MjE2OTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.aE9EVt619DQO-Z8PufAEHuuZRmUzHBKFfaTWWoREj_CdHY1TJQmjROvxKZNJMBULQ9d-ZhslG3L8qqtvyjmu8Q"}
     # r = requests.get(url,headers=headers)
     # print(r.text)
-    r = requests_get(url,headers=headers)
+    # r = requests_get(url,headers=headers)
+    request = Request()
+    r = request.get(url,headers=headers)
     print(r)
 
 def goodlist():
     url = "https://appdev.xpandago.com/app/goods/brand/brand/goodlist"
     data = {"brandId":"286","sidx":"person","order":"desc","page":"1"}
-    r = requests.get(url,params=data)
-    print(r.text)
+    # r = requests.get(url,params=data)
+    # print(r.text)
+    # r = requests_get(url,params=data)
+    request = Request()
+    r = request.get(url,params=data)
+    print(r)
 
 def cart():
     url = "https://appdev.xpandago.com/app/shop/cart/buycar/saveNew"
@@ -28,18 +40,24 @@ def cart():
     # r = requests.post(url,data=data,headers=headers)
     # print(r.text)
     # print(r.json())
-    r = requests_post(url,data=data,headers=headers)
+    # r = requests_post(url,data=data,headers=headers)
+    request = Request()
+    r = request.post(url,data=data,headers=headers)
     print(r)
 
 def orderNo():
     url = "https://appdev.xpandago.com/app/order/order/order/addOrderToRedisNew"
-    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA2MjgyMjEyMjQ3LCJleHAiOjE2MDY4ODcwMTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.6iCaPo7vqVSXUIPM0ciyYpvh25TRZSoqb0YcBtTCob3NzxZ1WuW6sN87NsV7UxrhWHVpEoJfVWb5vi7Cu1nhAA"}
-    r = requests.get(url,headers=headers)
-    print(r.text)
+    headers = {"token":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiLmtYvor5UtVEciLCJhdWRpZW5jZSI6ImUxMGFkYzM5NDliYTU5YWJiZTU2ZTA1N2YyMGY4ODNlIiwidXNlcl9pZCI6MjUwNjYzLCJjcmVhdGVkIjoxNjA5OTE2ODkyMzk1LCJleHAiOjE2MTA1MjE2OTIsInVzZXJfZ3JvdXBfaWQiOm51bGx9.aE9EVt619DQO-Z8PufAEHuuZRmUzHBKFfaTWWoREj_CdHY1TJQmjROvxKZNJMBULQ9d-ZhslG3L8qqtvyjmu8Q"}
+    # r = requests.get(url,headers=headers)
+    # print(r.text)
+    # r = requests_get(url,headers=headers)
+    request = Request()
+    r = request.get(url,headers=headers)
+    print(r)
 
 if __name__ == '__main__':
-    # login()
-    # info()
-    # goodlist()
+    login()
+    info()
+    goodlist()
     cart()
-    # orderNo()
+    orderNo()
--- utils/RequestsUtils.py
+++ utils/RequestsUtils.py
@@ -1,10 +1,9 @@
 
 import requests
-# 创建封装方法
+# get方法封装
 def requests_get(url, params=None, **kwargs):
 # 发送requests get请求
     r = requests.get(url,params=params,**kwargs)
-# 获取相应结果
     code = r.status_code
     try:
         body = r.json()
@@ -14,11 +13,10 @@ def requests_get(url, params=None, **kwargs):
     res = dict()
     res["code"] = code
     res["body"] = body
-# 字典返回
+
     return res
 
 # post方法封装
-# 创建post方法**kwargs
 def requests_post(url, data=None, json=None, **kwargs):
     r = requests.post(url, data=data, json=json, **kwargs)
     code = r.status_code
@@ -31,3 +29,29 @@ def requests_post(url, data=None, json=None, **kwargs):
     res["code"] = code
     res["body"] = body
     return res
+
+class Request:
+    # 定义公共方法
+    def requests_api(self,url, params=None, data=None, json=None,method='get',**kwargs,):
+        if method == "get":
+            # get请求
+            r = requests.get(url, params=params, **kwargs)
+        elif method == "post":
+            # post请求
+            r = requests.post(url, data=data, json=json, **kwargs)
+        code = r.status_code
+        try:
+            body = r.json()
+        except Exception as e:
+            body = r.text
+        # body = r.text
+        res = dict()
+        res["code"] = code
+        res["body"] = body
+        return res
+    # 重构get方法
+    def get(self,url,**kwargs):
+        return self.requests_api(url,method="get",**kwargs)
+    # 重构post方法
+    def post(self,url,**kwargs):
+        return self.requests_api(url,method="post",**kwargs)
