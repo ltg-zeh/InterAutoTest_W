fc00b56d4f5d6c379796e70e02fd62f0c08d44ee
ltg <ltgxd2h@163.com>
2021-01-08 17:50:52 +0800

    读取多文件

--- testcase/t_yaml/yaml_demo.py
+++ testcase/t_yaml/yaml_demo.py
@@ -3,7 +3,14 @@
 # 3.输出文件
 
 import yaml
+# 读取单个文件
+# with open("./data.yaml",encoding='utf-8') as f:
+#     r = yaml.safe_load(f)
+#     print(r)
 
-with open("./data.yaml",encoding='utf-8') as f:
-    r = yaml.safe_load(f)
-    print(r)
+# 读取多个文件
+with open("./data.yaml","r",encoding='utf-8') as f:
+
+    r = yaml.safe_load_all(f)
+    for i in r:
+        print(i)
